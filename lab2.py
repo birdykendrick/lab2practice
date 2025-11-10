e739a39cc7356d64de0e2f2323094f7ffb48cf90
Kendrick <khookendrick@gmail.com>
2025-11-10 21:35:51 +0800

    BMI

--- lab2.py
+++ lab2.py
@@ -1,1 +1,16 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight /((height * height))
+
+    print("BMI = " + str(bmi))
+
+    if bmi < 18.5:
+        print("You are underweight.")
+    elif bmi >= 18.5 and bmi <= 25:
+        print("You have a normal weight.")
+    elif bmi > 25:
+        print("You are overweight.")
+        
+calculate_bmi(weight=57, height=1.73)
