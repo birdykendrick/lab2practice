70cf50ff3f790adf7e756f107c9f8882f69d617a
Kendrick <khookendrick@gmail.com>
2025-11-10 22:06:13 +0800

    Final for lab2

--- lab2.py
+++ lab2.py
@@ -12,5 +12,32 @@ def calculate_bmi(height, weight):
         print("You have a normal weight.")
     elif bmi > 25:
         print("You are overweight.")
-        
+
 calculate_bmi(weight=57, height=1.73)
+
+def calc_average(float_list):
+    total = 0
+    for num in float_list:
+        total = total + sum(num)
+        average = total / len(float_list)
+    return average
+
+def find_min_max(float_list):
+    minvalue = min(float_list)
+    maxvalue= max(float_list)
+    return(minvalue, maxvalue)
+
+def sort_temperatures(float_list):
+    float_list.sort()
+    return float_list
+
+def calc_median(float_list):
+    float_list.sort()
+    length = len(float_list)
+    if length % 2 == 0:
+        median = (float_list[length // 2 - 1] + float_list[length // 2]) / 2
+    else:
+        median = float_list[length // 2]
+    return median
+        
+    
