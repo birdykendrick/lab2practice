2b8a5cf947987a04fa22de05b931d5c72585a123
Kendrick <khookendrick@gmail.com>
2025-11-28 19:59:26 +0800

    update return values

--- bmi.py
+++ bmi.py
@@ -6,11 +6,11 @@ def calculate_bmi(height, weight):
     print("Your BMI is: " + str(bmi))
 
     if bmi < 18.5:
-        print ("Under weight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25:
-        print("Normal weight")
+        return 0
     else:
-        print("Overweight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
 
